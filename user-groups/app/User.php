87ad031ac8c059f420957bb3f6fb071904293924
Anita <anita.kapal@inscripts.in>
2021-01-20 22:38:46 +0530

    created user APIs

---
+++ user-groups/app/User.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'contact_no',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+}
